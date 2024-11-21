667e251d786a63bd1d23c6c5d12b063fa40047a4
sonyijay <132740582+sonyijay@users.noreply.github.com>
2024-11-21 12:42:08 +0530

    Add files via upload
    Day2

---
+++ java/Story.java
@@ -0,0 +1,23 @@
+public class Story
+{
+public static void main (String args[])
+{
+String name1="Sony.V";
+String adjective1="happy";
+String adjective2="wonderful";
+String noun1="birds";
+String noun2="chocolate";
+String verb1="jump";
+String noun3="guitar";
+String noun4="children";
+String adjective3="excited";
+String name2="sam";
+String place1="Hawali";
+String noun5="ice cream";
+int number= 2050;
+String noun6="robots";
+
+String story = "This morning " + name1 + " woke up feeling " + adjective1 + ". 'It is going to be a " + adjective2+ " day!' Outside, a bunch of " + noun1+ "s were protesting to keep " + noun2 + " in stores. They began to" + verb1 + " to the rhythm of the " + noun3 + ", which made all the " + noun4 + "s very " + adjective3 + ".Concerned, " + name1 + " texted " + name2 + ", who flew " + name1 + " to " + place1 + " and dropped " +name1 + " in a puddle of frozen "+noun5 + ". " + name1 + " woke up in the year " + number + ", in a worldwhere " + noun6 + "s ruled the world.";
+System.out.println(story);
+}
+}

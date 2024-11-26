54bd66eedc3a0cc3a5cab8bdf0d9f370b2dd83ad
sonyijay <132740582+sonyijay@users.noreply.github.com>
2024-11-26 13:11:58 +0530

    Add files via upload
    Day5

---
+++ java/Loop.java
@@ -0,0 +1,41 @@
+package day2;
+
+import java.util.Scanner;
+
+public class Loop {
+	public static void main(String args[]) {
+//		
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("enter your mark");
+//			
+//		
+//	int mark=sc.nextInt();
+//	if(mark>=35) {
+//		System.out.println("pass");
+//	if(mark>=90)
+//		System.out.println("grade A");
+//	if(mark>=70)
+//		System.out.println("grade B");
+//		
+//	} else {
+//		System.out.println("fail");
+//	}
+//	
+//	
+//		 
+// 
+//		
+//	
+//	
+//
+//}
+//} 
+//  
+		Scanner sc = new Scanner(System.in);
+		int table=sc.nextInt();		
+		
+		for(int i=1;i<=10;i++) {
+	System.out.println("table"+i+"="+i*table);
+}
+}
+}

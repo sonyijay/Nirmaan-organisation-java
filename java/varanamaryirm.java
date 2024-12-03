b10bae77fce235a69041be1a30119de75b0c6d7c
sonyijay <132740582+sonyijay@users.noreply.github.com>
2024-12-03 13:12:21 +0530

    day8

---
+++ java/varanamaryirm.java
@@ -0,0 +1,36 @@
+package day7;
+//import java.util.Scanner;
+
+public class varanamaryirm {
+	public static void main(String[] args) {
+//		Scanner sc= new Scanner(System.in);
+//		{
+//		System.out.println(" enter the status");	
+//		}
+//
+//		String ramya=sc.nextLine();
+//		String status=new String("alive");
+//			
+//			if(ramya.equals(status)) {
+//				System.out.println("surya weds ramya");}
+//				else {
+//					System.out.println("surya weds megana");
+//				}
+//				
+//		}
+//		
+//		
+//	}
+     for(int i=0;i<=3;i++) {
+ for (int j=0;j<=3;j++) {
+	 System.out.print("*");
+ }
+     
+ 
+	 System.out.println();
+     }
+ 
+     
+	}}
+
+	
